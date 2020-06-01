59bac323772d4f484f63a511a3a55e09efef385c
fargusMD <bragaruion@gmail.com>
2020-06-01 11:58:01 +0400

    Updates

---
+++ precache-manifest.b512100b2144e72d31e1a04dccdf6db1.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c4b05ed94bc7225d06a2227fca7cd132",
+    "url": "/travel-demo-front/index.html"
+  },
+  {
+    "revision": "71c9db7cf858083f3b25",
+    "url": "/travel-demo-front/static/css/main.b793da3b.chunk.css"
+  },
+  {
+    "revision": "775f706c6cab3737fcee",
+    "url": "/travel-demo-front/static/js/2.4691620f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/travel-demo-front/static/js/2.4691620f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "71c9db7cf858083f3b25",
+    "url": "/travel-demo-front/static/js/main.df4c10af.chunk.js"
+  },
+  {
+    "revision": "5e109dc8b8ee2941438a",
+    "url": "/travel-demo-front/static/js/runtime-main.dc7014b3.js"
+  },
+  {
+    "revision": "7f0611380159f970ef35d19a4546c30d",
+    "url": "/travel-demo-front/static/media/1.7f061138.jpg"
+  },
+  {
+    "revision": "1282e172d89b8b009db49947de647029",
+    "url": "/travel-demo-front/static/media/2.1282e172.jpg"
+  },
+  {
+    "revision": "7f63313e0a1dab18d80702884be6c299",
+    "url": "/travel-demo-front/static/media/3.7f63313e.jpg"
+  },
+  {
+    "revision": "c9cfed80dac60aa2a0c17754fd6be7ef",
+    "url": "/travel-demo-front/static/media/4.c9cfed80.jpg"
+  },
+  {
+    "revision": "6ed19f26527fb168c53e39f62ea87450",
+    "url": "/travel-demo-front/static/media/5.6ed19f26.jpg"
+  },
+  {
+    "revision": "49b32ad2e5224b8029cd49563ffb911c",
+    "url": "/travel-demo-front/static/media/6.49b32ad2.jpg"
+  },
+  {
+    "revision": "d334349f85e3d11b9076e8a123d16721",
+    "url": "/travel-demo-front/static/media/7.d334349f.jpg"
+  },
+  {
+    "revision": "30229760759e7bfdeb5eb47ede5afba7",
+    "url": "/travel-demo-front/static/media/card1.30229760.jpg"
+  },
+  {
+    "revision": "f908bd46456073ae0b675634d3058e47",
+    "url": "/travel-demo-front/static/media/card2.f908bd46.jpg"
+  },
+  {
+    "revision": "9b2a6c605f4fa389b825becf24c0ed8e",
+    "url": "/travel-demo-front/static/media/card3.9b2a6c60.jpg"
+  },
+  {
+    "revision": "4e6c5cb267b690817aa5753e5f454b85",
+    "url": "/travel-demo-front/static/media/hero-1.4e6c5cb2.jpg"
+  },
+  {
+    "revision": "00258f1296fa39c359072e67ecf18075",
+    "url": "/travel-demo-front/static/media/testimonials.00258f12.jpg"
+  }
+]);
